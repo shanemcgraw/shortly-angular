29e87498b73726cb18b4532929247ecbdd6899b0
shanemcgraw <shanemcgraw17@gmail.com>
2016-02-12 19:03:12 -0800

    Post new link to server

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,9 +2,16 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, $http) {
 
-  $scope.postLink = function () {
-    console.log("hi");
+  $scope.postLink = function (newLink) {
+    console.log('Post from client', newLink);
 
+    var postData = JSON.stringify( {url: newLink} );
+
+    $http.post("http://localhost:3000/api/links", postData)
+      .success(function(response){ 
+        console.log('success!', response);
+      });
+  
   };
 
 });
