34686aff18059280c10861e93b124b3efa8457d2
Brian Fogg <fogg4444@gmail.com>
2016-02-13 16:16:59 -0800

    Redirect to links on unknown url path

--- client/app/app.js
+++ client/app/app.js
@@ -22,17 +22,17 @@ angular.module('shortly', [
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
+      controller: 'ShortenController',
+      authenticate: true
     })
     .otherwise({
-      templateUrl: 'app/auth/links.html',
-      controller: 'AuthController'
+      redirectTo: '/links'
     })
-
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
